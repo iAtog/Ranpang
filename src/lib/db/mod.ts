9403bcf98bc5036b56d98b33f613d500c5e8ca37
iAtog <atogmc58vani@gmail.com>
2024-10-14 00:51:04 -0400

    database lib

--- src/lib/db/mod.ts
+++ src/lib/db/mod.ts
@@ -1,3 +1,52 @@
+import { Team } from "../team/mod.ts";
+
 abstract class Database {
-    public abstract get(key: string): Promise<string>;
+    public abstract get(key: string): Promise<Team>;
+    public abstract remove(key: string): Promise<Team>;
+    public abstract save(key: string, value: Team): Promise<Team>;
+    public abstract getAll(): Promise<Team[]>;
+}
+
+class MongoDB extends Database {
+    public get(key: string): Promise<Team> {
+        return Promise.resolve(undefined!);
+    }
+
+    public remove(key: string): Promise<Team> {
+        return Promise.resolve(undefined!);
+    }
+
+    public save(key: string, value: Team): Promise<Team> {
+        return Promise.resolve(undefined!);
+        //return this.teams.set(key, value);
+    }
+
+    public getAll(): Promise<Team[]> {
+        return Promise.resolve([]);
+    }
+}
+
+class LocalDatabase extends Database {
+    public get(key: string): Promise<Team> {
+        return Promise.resolve(undefined!);
+    }
+
+    public remove(key: string): Promise<Team> {
+        return Promise.resolve(undefined!);
+    }
+
+    public save(key: string, value: Team): Promise<Team> {
+        return Promise.resolve(undefined!);
+        //return this.teams.set(key, value);
+    }
+
+    public getAll(): Promise<Team[]> {
+        return Promise.resolve([]);
+    }
+}
+
+export {
+    Database,
+    MongoDB,
+    LocalDatabase
 }
