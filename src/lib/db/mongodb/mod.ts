db912e88e6bccf763a661b7da7ca8b798c25731a
iAtog <atogmc58vani@gmail.com>
2024-10-15 02:25:58 -0400

    implement mongodb class

--- src/lib/db/mongodb/mod.ts
+++ src/lib/db/mongodb/mod.ts
@@ -1,29 +1,96 @@
+import mongoose from "npm:mongoose";
+import type { Connection, Mongoose } from "npm:mongoose";
+import schema from './schema.ts';
+
 import { Database } from "../mod.ts";
 import { Team } from "../../team/mod.ts";
 
 class MongoDB extends Database {
-    public get(key: string): Promise<Team> {
-        return Promise.resolve(undefined!);
+
+    public connection: Mongoose | undefined;
+    
+    constructor() {
+        super();
     }
 
-    public remove(key: string): Promise<Team> {
-        return Promise.resolve(undefined!);
+    public get(key: string): Promise<Team> {
+        return new Promise((resolve, reject) => {
+            try {
+                schema.findOne({id: key }).then(data => {
+                    resolve(data?.toJSON() as Team);
+                }).catch((e) => {
+                    resolve(undefined!);
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    public remove(key: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            try {
+                schema.deleteOne({id: key }).then(() => {
+                    resolve();
+                }).catch((e) => {
+                    resolve();
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
     }
 
     public set(key: string, value: Team): Promise<boolean> {
-        return Promise.resolve(undefined!);
+        return new Promise((resolve, _reject) => {
+            new schema({
+                id: key,
+                gamemode: value.gamemode.toString().toUpperCase(),
+                description: value.description,
+                members: value.members,
+                screenshots: value.screenshots,
+                createdAt: value.createdAt,
+                author: value.author
+            }).save().then(() => {
+                resolve(true);
+            }).catch((e) => {
+                console.log(e);
+                resolve(false);
+            })
+        });
     }
 
     public getAll(): Promise<Team[]> {
-        return Promise.resolve([]);
+        return new Promise((resolve, _reject) => {
+            schema.find({}).then(data => {
+                resolve(data.map(d => d.toJSON() as Team));
+            }).catch(e => {
+                console.log(e);
+                resolve([]);
+            });
+        });
     }
 
-    public connect(): Promise<void> {
+    public async connect(): Promise<void> {
+        this.connection = await mongoose.connect(Deno.env.get("MONGODB_URI")!);
+        console.log("Connected to MongoDB!");
         return Promise.resolve();
     }
 
     public close(): Promise<boolean> {
-        return Promise.resolve(true);
+        return new Promise((resolve, _reject) => {
+            mongoose.connection.close().then(() => {
+                resolve(true);
+                console.log("Closed MongoDB!");
+            }).catch(e => {
+                console.log(e);
+                resolve(false);
+            });
+        })
+    }
+
+    public override save(): Promise<void> {
+        return Promise.resolve();
     }
 }
 
