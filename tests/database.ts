431c005b970da803b1294553e52bf7c6ad9e0f78
iAtog <atogmc58vani@gmail.com>
2024-10-15 04:18:51 -0400

    change test to mongodb

--- tests/database.ts
+++ tests/database.ts
@@ -1,5 +1,8 @@
+import "jsr:@std/dotenv/load"
+
 import { expect } from "jsr:@std/expect";
-import LocalDatabase from "../src/lib/db/local/mod.ts";
+import _LocalDatabase from "../src/lib/db/local/mod.ts";
+import MongoDB from "../src/lib/db/mongodb/mod.ts";
 import type { Database } from "../src/lib/db/mod.ts";
 import type { Screenshot } from "../src/lib/team/mod.ts";
 import { TeamGameMode } from "../src/lib/team/mod.ts";
@@ -7,20 +10,17 @@ import { TeamGameMode } from "../src/lib/team/mod.ts";
 let database: Database;
 
 Deno.test("define database", () => {
-    database = new LocalDatabase();
+    database = new MongoDB();
     expect(database).toBeDefined();
 });
 
 Deno.test("connect database", async() => {
     await database.connect();
-    expect(await database.getAll()).toBeDefined();
+    expect(database.isConnected()).toBeTruthy();
 });
 
-Deno.test("add team", () => {
-    const screenshots: Screenshot[] = [];
-    screenshots.push({url: "test", name: "test"});
-    
-    database.set("test", {
+Deno.test("add team", async() => {
+    await database.set("etest1", {
         id: "etest1",
         description: "testing",
         gamemode: TeamGameMode.COLOSSEUM,
@@ -32,22 +32,21 @@ Deno.test("add team", () => {
         ],
         author: "Richard",
         createdAt: new Date(),
-        screenshots
+        screenshots: [{url: "test", name: "test"}]
     });
 
-    expect(database.get("test")).toBeDefined();
+    expect(database.get("etest1")).toBeDefined();
 });
 
 Deno.test("async check team", async() => {
-    const team = await database.get("test");
-    expect(team).toBeDefined();
+    const team = await database.get("etest1");
     expect(team?.id).toBe("etest1");
 });
-
+/*
 Deno.test("async remove team", async() => {
     await database.remove("test");
     expect(await database.get("test")).toBeUndefined();
-})
+})*/
 
 Deno.test("async close database", async() => {
     const closed = await database.close();
