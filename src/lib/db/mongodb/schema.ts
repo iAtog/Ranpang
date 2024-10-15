5998167276c92ba014c43d14d330421ae42016fd
iAtog <atogmc58vani@gmail.com>
2024-10-15 02:28:02 -0400

    finish mongoose schema

---
+++ src/lib/db/mongodb/schema.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from "npm:mongoose";
+
+const teamSchema = new Schema({
+    id: String,
+    gamemode: String,
+    description: String,
+    members: [],
+    screenshots: [],
+    createdAt: Date,
+    author: String
+}, { collection: "teams"});
+
+teamSchema.path("id").required(true, "id cannot be blank.");
+teamSchema.path("description").required(true, "description cannot be blank.");
+teamSchema.path("gamemode").required(true, "gamemode cannot be blank.");
+teamSchema.path("author").required(true, "author cannot be blank.");
+
+export default model("Team", teamSchema);
