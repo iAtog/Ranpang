1c2dd8a19931129be10cff5ebc39073b34e07d5f
iAtog <atogmc58vani@gmail.com>
2024-10-16 00:07:04 -0400

    upgrade subcommands

--- src/commands/gt/teams.ts
+++ src/commands/gt/teams.ts
@@ -1,7 +1,8 @@
 
-import {Command, ChoiceType} from "../../class/Command.ts";
+import { Command, ChoiceType } from "../../class/Command.ts";
 import { Client, CommandInteraction } from "npm:discord.js";
 import { Embed } from "npm:@notenoughupdates/discord.js-builders";
+import { TeamsHandler } from "../../lib/team/mod.ts";
 
 class Help extends Command {
     constructor() {
@@ -44,28 +45,48 @@ class Help extends Command {
                             autocomplete: true
                         },
                     ]
+                },
+                {
+                    type: ChoiceType.SUB_COMMAND,
+                    name: "crear",
+                    description: "Crear equipo nuevo"
                 }
             ]
         });
     }
 
     async run(client: Client, interaction: CommandInteraction) {
-        //const commands = client.commands.map(command => command.name);
-        const embed = new Embed()
+        if(!client.teams) {
+            interaction.reply({ content: ":x: Ocurrió un error en la carga de datos, por favor intenta nuevamente", ephemeral: true });
+            return;
+        }
+        /*const embed = new Embed()
             .setTitle("Commands")
-            .setDescription("hi");
-        if(interaction.options.data[0].name === "counter") {
-            const counter = interaction.options.data[0].options;
-            if(!counter) {
-                interaction.reply({ content: "Error: No se encontró el grupo" });
-                return;
-            };
-            const leader = counter[0]?.value?.toString();
-            const member2 = counter[1]?.value?.toString();
-            const member3 = counter[2]?.value?.toString();
-            const member4 = counter[3]?.value?.toString();
-            
-        }   
+            .setDescription("hi");*/
+        const teamsHandler = client.teams as TeamsHandler;
+
+        if (interaction.options.data[0].name === "counter") {
+            this.counterSubcommand(client, interaction, teamsHandler);
+        } else if (interaction.options.data[0].name === "crear") {
+            this.crearSubcommand(client, interaction, teamsHandler);
+        }
+    }
+
+    public counterSubcommand(client: Client, interaction: CommandInteraction, teamsHandler: TeamsHandler) {
+        const counter = interaction.options.data[0].options;
+        if (!counter) {
+            interaction.reply({ content: "Error: No se encontró el grupo" });
+            return;
+        };
+        const leader = counter[0]?.value?.toString();
+        const member2 = counter[1]?.value?.toString();
+        const member3 = counter[2]?.value?.toString();
+        const member4 = counter[3]?.value?.toString();
+        
+    }
+
+    public crearSubcommand(client: Client, interaction: CommandInteraction, teamsHandler: TeamsHandler) {
+
     }
 
     public override async autocomplete(interaction: any): Promise<void> {
