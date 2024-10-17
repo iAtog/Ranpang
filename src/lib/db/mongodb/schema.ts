1dd475eb37d8443049cd31b9a98514a47e037017
iAtog <atogmc58vani@gmail.com>
2024-10-17 00:21:47 -0400

    fixed schema

--- src/lib/db/mongodb/schema.ts
+++ src/lib/db/mongodb/schema.ts
@@ -12,6 +12,5 @@ const teamSchema = new mongoose.Schema({
 teamSchema.path("id").required(true, "id cannot be blank.");
 teamSchema.path("description").required(true, "description cannot be blank.");
 teamSchema.path("gamemode").required(true, "gamemode cannot be blank.");
-teamSchema.path("author").required(true, "author cannot be blank.");
 
 export default mongoose.model("Team", teamSchema);
