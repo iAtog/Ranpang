718d05f2b0b103eb935453797db67d3557d3554d
iAtog <atogmc58vani@gmail.com>
2024-10-17 07:47:58 -0400

    improved hero embed and creation

--- src/lib/team/mod.ts
+++ src/lib/team/mod.ts
@@ -1,5 +1,6 @@
 import { Database } from "../db/mod.ts";
-import { Embed } from "npm:@notenoughupdates/discord.js-builders";
+import { Embed, ButtonComponent, ActionRowComponent } from "npm:@notenoughupdates/discord.js-builders";
+import { ButtonBuilder,  ActionRowBuilder, ButtonStyle } from "npm:discord.js";
 
 class TeamsHandler {
     private database: Database;
@@ -20,81 +21,131 @@ class TeamsHandler {
         this.database.save();
     }
 
-    public async addTeam(team: Team): Promise<boolean>{
+    public async addTeam(team: Team): Promise<boolean> {
         return await this.database.set(team.id, team);
     }
 
     public createTeamEmbed(team: Team): Embed {
         const embed = new Embed()
-            .setTitle("Equipo #`" + team.id + "`")
-            .setDescription(team.description)
-            .setColor(0x0a53ab)
-            .setFooter({ text: team.id});
+            .setTitle("Equipo #``" + team.id + "`` (" + this.mayus(team.type.toLowerCase()) + ")")
+            .setDescription("**" + team.description + "**")
+            .setColor(0x0eb482)
+            .setFooter({ text: team.id });
 
-        embed.addField({name: "Miembros del equipo", value: team.members.map(member => member.name).join(", "), inline: true});
-        if(team.screenshots.length > 0) {
+        embed.addFields({ 
+            name: "Miembros", 
+            value: team.members.map(member => member.name).join("\n"), 
+            inline: true 
+        }, {
+            name: "Modo de juego",
+            value: this.mayus(team.gamemode.toLowerCase()),
+            inline: true
+        });
+        if (team.screenshots.length > 0) {
             embed.setImage(team.screenshots[0].url);
         } else {
             embed.setImage("https://t3.ftcdn.net/jpg/04/84/88/76/360_F_484887682_Mx57wpHG4lKrPAG0y7Q8Q7bJ952J3TTO.jpg");
         }
+
+        embed.setTimestamp(team.createdAt);
+        embed.addFields({
+            name: "Imagen actual",
+            value: "1/" + team.screenshots.length,
+            inline: true
+        })
         return embed;
     }
 
-    public getCounter(members: TeamMember[]): Promise<Team> {
+    private mayus(str: string) {
+        let newStr = "";
+
+        newStr += str.charAt(0).toUpperCase();
+        newStr += str.slice(1);
+
+        return newStr;
+    }
+
+    public getTeamByMembers(type: keyof typeof TeamType, members: TeamMember[]): Promise<Team> {
         return new Promise<Team>((resolve, _reject) => {
-            this.database.getAll().then(async(teams) => {
-                for(const team of teams) {
-                    if(team.members.length !== members.length) continue;
-                    if(members[0].key !== team.members[0].key) continue;
+            this.database.getAll().then(async (teams) => {
+                for (const team of teams) {
+                    if (team.type !== type.toString()) continue;
+                    if (team.members.length !== members.length) continue;
+                    if (members[0].key !== team.members[0].key) continue;
 
                     const companions1 = new Set(members.slice(1).map(member => member.key));
                     const companions2 = new Set(team.members.slice(1).map(member => member.key));
 
-                    if(companions1.size !== companions2.size) continue;
+                    if (companions1.size !== companions2.size) continue;
 
                     let isEqual = true;
 
                     for await (let key of companions1) {
                         if (!companions2.has(key)) {
-                          isEqual = false;
+                            isEqual = false;
                         }
                     }
 
-                    if(!isEqual) continue;
+                    if (!isEqual) continue;
 
                     resolve(team);
                     break;
                 }
-                
+
             })
         })
     }
-/*
-    public getTeam(members: TeamMember[]): Team {
-        for (let i = 0; i < this.teams.length; i++) {
-            const team = this.teams[i];
-            if (team.members.length === members.length) {
-                let isEqual = true;
-                for (let j = 0; j < team.members.length; j++) {
-                    if (team.members[j].key !== members[j].key) {
-                        isEqual = false;
-                        break;
+
+    public TwoStarHeros(): string[] {
+        return [
+            "eva", "whiteBeast", "loraine", "lavi", "favi", "aoba", "gremory", "rachel", "hekate",
+            "marianne", "akayuki", "ranpang", "yuze", "aisha", "shapira", "dolf", "amy", "catherine"
+        ]
+    }
+
+    public getScrollButtons(): ActionRowBuilder {
+        const scrollback = new ButtonBuilder() as unknown as any;
+        scrollback.setCustomId("back");
+        scrollback.setLabel("◀");
+        scrollback.setStyle(ButtonStyle.Primary)
+
+        const scrollforward = new ButtonBuilder() as unknown as any;
+        scrollforward.setCustomId("forward")
+        scrollforward.setLabel("▶");
+        scrollforward.setStyle(ButtonStyle.Primary)
+
+        const row = new ActionRowBuilder() as unknown as any;
+	    row.addComponents(scrollback, scrollforward);
+
+        return row;
+    }
+    /*
+        public getTeam(members: TeamMember[]): Team {
+            for (let i = 0; i < this.teams.length; i++) {
+                const team = this.teams[i];
+                if (team.members.length === members.length) {
+                    let isEqual = true;
+                    for (let j = 0; j < team.members.length; j++) {
+                        if (team.members[j].key !== members[j].key) {
+                            isEqual = false;
+                            break;
+                        }
+                    }
+                    if (isEqual) {
+                        return team;
                     }
                 }
-                if (isEqual) {
-                    return team;
-                }
             }
-        }
-        //this.createTeam("COLOSSEUM", "Test", [{key: "test", name: "Test"}]);
-        return undefined!; 
-    }*/
+            //this.createTeam("COLOSSEUM", "Test", [{key: "test", name: "Test"}]);
+            return undefined!; 
+        }*/
 
-    public createTeamObject(gamemode: TeamGameMode, description: string, members: TeamMember[], screenshots: Screenshot[] = []): Team {
+    public createTeamObject(type: keyof typeof TeamType, gamemode: TeamGameMode, description: string, members: TeamMember[], screenshots: Screenshot[] = []): Team {
         const id = createTeamId();
 
         return {
             id,
+            type,
             gamemode,
             description,
             members,
@@ -111,7 +162,7 @@ class TeamsHandler {
         const members: TeamMember[] = [];
         for await (const hero of heros) {
             const heroApi = await this.getHero(hero);
-            if(heroApi) {
+            if (heroApi) {
                 members.push({
                     key: heroApi.key,
                     name: heroApi.name
@@ -122,11 +173,11 @@ class TeamsHandler {
         return members;
     }
 
-    public async getHero(name: string): Promise<Heroe> { 
+    public async getHero(name: string): Promise<Heroe> {
         const response = await fetch("https://www.gtales.top/api/heroes?hero=" + name);
         const data = await response.json();
-        
-        if(data.error) {
+
+        if (data.error) {
             return undefined!;
         }
 
@@ -137,10 +188,10 @@ class TeamsHandler {
         const response = await fetch("https://www.gtales.top/api/heroes");
         const data = await response.json();
 
-        if(data.error) {
+        if (data.error) {
             return [];
         }
-        
+
         return data as TeamMember[];
     }
 }
@@ -155,8 +206,14 @@ function createTeamId(): string {
     return id;
 }
 
+enum TeamType {
+    COUNTERS = "COUNTERS",
+    PRESET = "PRESET"
+}
+
 interface Team {
     id: string;
+    type: string;
     gamemode: TeamGameMode;
     description: string;
     members: TeamMember[];
