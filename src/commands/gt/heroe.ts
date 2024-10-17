13d8ca21627660b9211c524a879b8818eb093f88
iAtog <atogmc58vani@gmail.com>
2024-10-17 00:21:24 -0400

    improved hero command

--- src/commands/gt/heroe.ts
+++ src/commands/gt/heroe.ts
@@ -2,6 +2,7 @@ import { Command, ChoiceType } from "../../class/Command.ts";
 import { Client, CommandInteraction, AutocompleteInteraction } from "npm:discord.js";
 import { Embed } from "npm:@notenoughupdates/discord.js-builders";
 import { Heroe } from "../../lib/team/mod.ts";
+import { TeamsHandler } from "../../lib/team/mod.ts";
 
 class CMD extends Command {
     private heroColor: number[];
@@ -23,46 +24,42 @@ class CMD extends Command {
         this.heroColor = [0xed3f13, 0x2789e6, 0xd1b724];
     }
 
-    // deno-lint-ignore no-unused-vars
     public async run(client: Client, interaction: CommandInteraction) {
-
+        const handler = client.teams as TeamsHandler;
         const heroName = interaction.options.get("name")?.value?.toString();
 
-        const response = await fetch("https://www.gtales.top/api/heroes?hero=" + heroName);
-        const dataRes = await response.json();
-
-        if (dataRes.error || !dataRes) {
+        const hero = await handler.getHero(heroName!);
+        if (hero === undefined) {
             interaction.reply({ content: "No se encontró el heroe `" + heroName + "`.", ephemeral: true });
             return;
         }
 
-        if(dataRes.warning) {
+        // deno-lint-ignore no-explicit-any
+        if((hero as unknown as any).warning!) {
             interaction.reply({ content: "El heroe `" + heroName + "` no está disponible aún.", ephemeral: true });
             return;
         }
 
-        const data = dataRes as Heroe;
-
         const embed = new Embed()
-            .setTitle(this.mayus(data.name))
-            .setDescription("[Party] " + data.party)
-            .setThumbnail("https://www.gtales.top/_next/image?url=%2Fassets%2Fheroes%2F" + data.atr + ".webp&w=256&q=75")
+            .setTitle(this.mayus(hero.name))
+            .setDescription("[Party] " + hero.party)
+            .setThumbnail("https://www.gtales.top/_next/image?url=%2Fassets%2Fheroes%2F" + hero.atr + ".webp&w=256&q=75")
             .addFields(
-                { name: "Role", value: this.mayus(data.role), inline: true },
-                { name: "Collection", value: this.mayus(data.collection), inline: true },
-                { name: "Element", value: this.mayus(data.element), inline: true },
-                { name: "Rarity", value: this.mayus(data.rarity) + " star" + (parseInt(data.rarity) > 1 ? "s" : ""), inline: true },
-                { name: "Available", value: data.available ? "Yes" : "No", inline: true }
+                { name: "Role", value: this.mayus(hero.role), inline: true },
+                { name: "Collection", value: this.mayus(hero.collection), inline: true },
+                { name: "Element", value: this.mayus(hero.element), inline: true },
+                { name: "Rarity", value: this.mayus(hero.rarity) + " star" + (parseInt(hero.rarity) > 1 ? "s" : ""), inline: true },
+                { name: "Available", value: hero.available ? "Yes" : "No", inline: true }
             )
             //.setTimestamp(Date.now())
-            .setFooter({ text: "Powered by gtales.top (only english) | " + data.stats.hp + " HP - " + data.stats.atk + " ATK - " + data.stats.def + " DEF" });
+            .setFooter({ text: "Powered by gtales.top (only english) | " + hero.stats.hp + " HP - " + hero.stats.atk + " ATK - " + hero.stats.def + " DEF" });
         
-        embed.setColor(data.rarity === "ascend" ? 0x25d60d : this.heroColor[parseInt(data.rarity) - 1]);
+        embed.setColor(hero.rarity === "ascend" ? 0x25d60d : this.heroColor[parseInt(hero.rarity) - 1]);
 
-        this.nonNull(data.na, () => embed.addFields({ name: "Normal Attack", value: data.na.title, inline: true }))
-        this.nonNull(data.ability, () => embed.addFields({ name: "Ability", value: data.ability.title, inline: true }))
-        this.nonNull(data.chain, () => embed.addFields({ name: "Chain", value: data.chain.title, inline: true }))
-        this.nonNull(data.weapon, () => embed.addFields({ name: "Weapon", value: data.weapon.name, inline: true }))
+        this.nonNull(hero.na, () => embed.addFields({ name: "Normal Attack", value: hero.na.title, inline: true }))
+        this.nonNull(hero.ability, () => embed.addFields({ name: "Ability", value: hero.ability.title, inline: true }))
+        this.nonNull(hero.chain, () => embed.addFields({ name: "Chain", value: hero.chain.title, inline: true }))
+        this.nonNull(hero.weapon, () => embed.addFields({ name: "Weapon", value: hero.weapon.name, inline: true }))
 
         interaction.reply({ embeds: [embed] });
     }
