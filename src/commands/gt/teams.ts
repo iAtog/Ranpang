a86312b50364b8cb1b492bbb0d9246dc78609495
iAtog <atogmc58vani@gmail.com>
2024-10-18 01:21:05 -0400

    implemented paginated embeds for teams

--- src/commands/gt/teams.ts
+++ src/commands/gt/teams.ts
@@ -4,6 +4,7 @@ import { Client, CommandInteraction, AutocompleteInteraction } from "npm:discord
 import { Embed } from "npm:@notenoughupdates/discord.js-builders";
 import { TeamGameMode, TeamsHandler, type Screenshot } from "../../lib/team/mod.ts";
 import EZ_Host from "../../lib/e-z_host/mod.ts";
+import { PaginatedEmbed } from "npm:embed-paginator";
 
 class Help extends Command {
     constructor() {
@@ -65,34 +66,34 @@ class Help extends Command {
                             description: "Descripción del equipo",
                             required: true
                         },
-                            {
-                                type: ChoiceType.STRING,
-                                name: "leader",
-                                description: "Lider del grupo",
-                                required: true,
-                                autocomplete: true
-                            },
-                            {
-                                type: ChoiceType.STRING,
-                                name: "member2",
-                                description: "Primer miembro",
-                                required: true,
-                                autocomplete: true
-                            },
-                            {
-                                type: ChoiceType.STRING,
-                                name: "member3",
-                                description: "Tercer miembro",
-                                required: true,
-                                autocomplete: true
-                            },
-                            {
-                                type: ChoiceType.STRING,
-                                name: "member4",
-                                description: "Cuarto miembro",
-                                required: true,
-                                autocomplete: true
-                            },
+                        {
+                            type: ChoiceType.STRING,
+                            name: "leader",
+                            description: "Lider del grupo",
+                            required: true,
+                            autocomplete: true
+                        },
+                        {
+                            type: ChoiceType.STRING,
+                            name: "member2",
+                            description: "Primer miembro",
+                            required: true,
+                            autocomplete: true
+                        },
+                        {
+                            type: ChoiceType.STRING,
+                            name: "member3",
+                            description: "Tercer miembro",
+                            required: true,
+                            autocomplete: true
+                        },
+                        {
+                            type: ChoiceType.STRING,
+                            name: "member4",
+                            description: "Cuarto miembro",
+                            required: true,
+                            autocomplete: true
+                        },
                         {
                             type: ChoiceType.ATTACHMENT,
                             name: "screenshot",
@@ -105,7 +106,7 @@ class Help extends Command {
     }
 
     async run(client: Client, interaction: CommandInteraction) {
-        if(!client.teams) {
+        if (!client.teams) {
             interaction.reply({ content: ":x: Ocurrió un error en la carga de datos, por favor intenta nuevamente", ephemeral: true });
             return;
         }
@@ -127,25 +128,37 @@ class Help extends Command {
             interaction.reply({ content: "Error: No se encontró el grupo" });
             return;
         };
-        interaction.reply({ content: "<a:loading:1296272884955877427> Consultando la base de datos..."})
+        interaction.reply({ content: "<a:loading:1296272884955877427> Consultando la base de datos..." })
         const leader = counter[0]?.value?.toString();
         const member2 = counter[1]?.value?.toString();
         const member3 = counter[2]?.value?.toString();
         const member4 = counter[3]?.value?.toString();
-        
+
         const teamMembers = await handler.generateTeamMembers([leader!, member2!, member3!, member4!]);
-        interaction.editReply({ content: "<a:loading:1296272884955877427> Creando instancia de equipo..."})
+        interaction.editReply({ content: "<a:loading:1296272884955877427> Creando instancia de equipo..." })
         const team = await handler.getTeamByMembers('COUNTERS', teamMembers);
 
-        if(!team) {
-            interaction.editReply({ content: ":x: Error: No se encontró el equipo."})
+        if (!team) {
+            interaction.editReply({ content: ":x: Error: No se encontró el equipo." })
             return;
         }
 
         const embed = handler.createTeamEmbed(team);
-        const rows = handler.getScrollButtons();
+        //const rows = handler.getScrollButtons() as any;
 
-        interaction.editReply({ embeds: [embed], content: "", components: [rows] });
+        await interaction.deleteReply();
+
+        const channel = client.channels.cache.get(interaction.channelId);
+        const paginatedEmbed = handler.createTeamEmbedPaginated(team);
+
+        await paginatedEmbed.send({
+            message: `<@${interaction.user.id}>`,
+            options: {
+                channel
+            }
+        })
+
+        //interaction.editReply({ embeds: [embed], content: "", components: [rows] });
     }
 
     public async crearSubcommand(client: Client, interaction: CommandInteraction, teamsHandler: TeamsHandler): Promise<void> {
@@ -163,26 +176,26 @@ class Help extends Command {
         await interaction.reply({ content: "<a:loading:1296272884955877427> Iniciando creación de equipo..." });
 
         let realGameMode: TeamGameMode;
-        if(gamemode === "COLOSSEUM") {
+        if (gamemode === "COLOSSEUM") {
             realGameMode = TeamGameMode.COLOSSEUM;
-        } else if(gamemode === "RAID") {
+        } else if (gamemode === "RAID") {
             realGameMode = TeamGameMode.RAID;
-        } else if(gamemode === "TETIS") {
+        } else if (gamemode === "TETIS") {
             realGameMode = TeamGameMode.TETIS;
         } else {
             interaction.editReply({ content: "Error: Modo de equipo no reconocido" });
             return;
         }
 
-        if(!gamemode || !description || !leader || !member2 || !member3 || !member4) {
+        if (!gamemode || !description || !leader || !member2 || !member3 || !member4) {
             interaction.editReply({ content: "Error: Faltan parámetros" });
             return;
         }
 
-        if(screenshot) {
-            interaction.editReply({ content: "<a:loading:1296272884955877427> Subiendo imagen al host..."});
+        if (screenshot) {
+            interaction.editReply({ content: "<a:loading:1296272884955877427> Subiendo imagen al host..." });
             const hostedUrl = await ezApi.createUrl(screenshot);
-            if(hostedUrl) {
+            if (hostedUrl) {
                 screenshots.push({
                     author: interaction.user.username,
                     url: hostedUrl
@@ -193,27 +206,27 @@ class Help extends Command {
             }
         }
 
-        interaction.editReply({ content: "<a:loading:1296272884955877427> Registrando el equipo..."});
+        interaction.editReply({ content: "<a:loading:1296272884955877427> Registrando el equipo..." });
         const members = await teamsHandler.generateTeamMembers([leader, member2, member3, member4]);
         const newTeam = await teamsHandler.createTeamObject("COUNTERS", realGameMode, description, members, screenshots);
 
         const success = await teamsHandler.addTeam(newTeam);
-        if(success) {
+        if (success) {
             const embed = teamsHandler.createTeamEmbed(newTeam);
             interaction.editReply({ content: "✅ Equipo registrado con éxito. ID: `" + newTeam.id + "`", embeds: [embed] });
         } else {
-            interaction.editReply({ content: ":x: Error: No se pudo registrar el equipo"});
+            interaction.editReply({ content: ":x: Error: No se pudo registrar el equipo" });
         }
     }
 
     public getPresets(interaction: CommandInteraction, teamsHandler: TeamsHandler): void {
 
     }
-        
-        
+
+
 
     public override async autocomplete(interaction: AutocompleteInteraction): Promise<void> {
-        if(interaction.options.getFocused(true).name === "gamemode") {
+        if (interaction.options.getFocused(true).name === "gamemode") {
             await this.gamemodeAutocomplete(interaction);
             return;
         }
